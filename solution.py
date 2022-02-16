6a4b6a2de889a3bca52a672d45521c60f901180e
Mohit Kumar Sharma <mohit@flux7.com>
2022-02-16 09:32:26 +0530

    Added code

--- solution.py
+++ solution.py
@@ -2,6 +2,7 @@
 from socket import *
 # In order to terminate the program
 import sys
+from urllib import response
 
 
 def webServer(port=13331):
@@ -9,27 +10,43 @@ def webServer(port=13331):
   #Prepare a server socket
   serverSocket.bind(("", port))
   #Fill in start
-
+  serverSocket.listen(1)
+  # print("Server is ready to listen the requests")
   #Fill in end
 
   while True:
     #Establish the connection
-    #print('Ready to serve...')
-    connectionSocket, addr = #Fill in start      #Fill in end
+    # print('Ready to serve...')
+    connectionSocket, addr = serverSocket.accept()
     try:
 
       try:
-        message = #Fill in start    #Fill in end
+        message = connectionSocket.recv(1024).decode()
         filename = message.split()[1]
         f = open(filename[1:])
-        outputdata = #Fill in start     #Fill in end
+        outputdata = f.read()
         
         #Send one HTTP header line into socket.
         #Fill in start
+        response_headers = {
+          'Content-Type': 'text/html; charset=UTF-8',
+          'Content-Length': len(outputdata)
+        }
+        response_headers_raw = ''.join(f"{k}: {v}\n" for k, v in response_headers.items())
+
+
+        server_http_version = 'HTTP/1.1'
+        status_code = '200'
+        status_phrase = 'OK'
+        http_status_line = f"{server_http_version} {status_code} {status_phrase}\n"
+        connectionSocket.send(http_status_line.encode())
+        connectionSocket.send(response_headers_raw.encode())
+        connectionSocket.send('\n'.encode())
 
         #Fill in end
 
         #Send the content of the requested file to the client
+        # print(outputdata.encode())
         for i in range(0, len(outputdata)):
           connectionSocket.send(outputdata[i].encode())
 
@@ -38,13 +55,18 @@ def webServer(port=13331):
       except IOError:
         # Send response message for file not found (404)
         #Fill in start
-
+        server_http_version = 'HTTP/1.1'
+        status_code = '404'
+        status_phrase = 'Not Found'
+        http_status_line = f"{server_http_version} {status_code} {status_phrase}\n"
+        connectionSocket.send(http_status_line.encode())
+        connectionSocket.send("\r\n".encode())
         #Fill in end
 
 
         #Close client socket
         #Fill in start
-
+        connectionSocket.close()
         #Fill in end
 
     except (ConnectionResetError, BrokenPipeError):
